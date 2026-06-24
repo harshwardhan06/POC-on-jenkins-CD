71e60360b23ab417930d4c8472885794e13b4048
harshwardhan06 <harshwardhan.dixit.snaatak@mygurukulam.co>
2026-06-24 06:54:40 +0530

    Update DeploymentExecutor.groovy

--- shared-library/src/com/company/cicd/DeploymentExecutor.groovy
+++ shared-library/src/com/company/cicd/DeploymentExecutor.groovy
@@ -8,21 +8,23 @@ class DeploymentExecutor implements Serializable {
         this.steps = steps
     }
 
-    void execute(List<String> apps) {
+    void execute(List<String> apps, Map<String, Map> configs) {
 
-        apps.each { app ->
+        apps.eachWithIndex { app, index ->
 
-            String imageTag =
-                ImageTagGenerator.generate(app)
+            Map cfg = configs[app]
 
             steps.echo "================================"
-            steps.echo "Application : ${app}"
-            steps.echo "Image Tag   : ${imageTag}"
+            steps.echo "Priority      : ${index + 1}"
+            steps.echo "Application   : ${app}"
+            steps.echo "Image Tag     : ${cfg.imageTag}"
+            steps.echo "Chart Version : ${cfg.chartVersion}"
             steps.echo "================================"
 
             steps.sh """
                 echo Deploying ${app}
-                echo Using tag ${imageTag}
+                echo Using image tag ${cfg.imageTag}
+                echo Using chart version ${cfg.chartVersion}
             """
         }
     }
